22546b349b62f522195369fec09da5ff857f70be
mario <qinghe0101@gmail.com>
2017-06-08 17:02:06 +0800

    test

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+<<<<<<< HEAD
   // res.render('index', { title: 'Express' });
   res.send('Hello Worldsadsad!');
+=======
+  res.render('index', { title: 'Express' });
+>>>>>>> test
 });
 
 module.exports = router;
