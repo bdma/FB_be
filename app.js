c3a333c491810126edb0feb004ce85305e11ee1f
mario <qinghe0101@gmail.com>
2017-06-14 15:35:02 +0800

    bd

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use(index);
+// app.use('/user', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
+  res.status(err.status || 500);
+  console.log(err.stack)
+  res.send(err.stack);
+  // res.send('errorsad');
   next(err);
 });
 
@@ -40,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var dbutile = require('./db');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+var url = 'mongodb://fbmongodb:BbH6PnuXgCvvXyMrZzUwzgD2tmCSFhFExD1Yf4i4LerOm33xBhCwFUoiraXlqKJE1NrVUmLJVeqqg89lizAGHw==@fbmongodb.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false';
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  // res.render('index', { title: 'Express' });
-  res.send('Hello Worldsadsad!');
-=======
-  res.render('index', { title: 'Express' });
->>>>>>> test
+MongoClient.connect(url, function (err, db) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  var collection = db.collection('documents');
+
+  router.get('/', function (req, res, next) {
+    // res.render('index', { title: 'Express' });
+    dbutile.find(collection, res)
+    // res.render('index', { title: 'Express' });
+  });
+  router.get('/user', function (req, res, next) {
+    res.send('respond with a resource');
+  });
+
+  // db.close();
 });
 
 module.exports = router;
